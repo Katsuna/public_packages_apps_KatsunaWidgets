449db5880f9b00f16b68d27f3180702cf95c07d7
nlarios <mqyVaXs2>
2017-05-29 17:27:11 +0300

    layout changes strings values updated

--- widgets/src/main/java/com/katsuna/widgets/weatherwidget/AlarmReceiver.java
+++ widgets/src/main/java/com/katsuna/widgets/weatherwidget/AlarmReceiver.java
@@ -240,23 +240,7 @@ public class AlarmReceiver extends BroadcastReceiver implements LocationListener
         }
     }
 
-    private void showLocationSettingsDialog() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
-        alertDialog.setTitle(R.string.location_settings);
-        alertDialog.setMessage(R.string.location_settings_message);
-        alertDialog.setPositiveButton(R.string.location_settings_button, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                context.startActivity(intent);
-            }
-        });
-        alertDialog.setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-        alertDialog.show();
-    }
+
 
     void getCityByLocation() {
         locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
